405619605a7c0b588c72e912db1002b6ef8649dd
Jahnavi <jahnavi.mathur@dotsquares.com>
2025-09-06 18:43:02 +0530

    changes

--- src/AdminModuleServiceProvider.php
+++ src/AdminModuleServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
 
 class AdminModuleServiceProvider extends ServiceProvider
 {
